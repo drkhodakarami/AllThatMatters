eb750d6182dea8831c544ea169303d9e9ada7349
Alireza Khodakarami <khodakarami_dr@outlook.com>
2023-11-09 21:42:15 +0330

    GemBow pulling duration is bugged

--- src/main/java/jiraiyah/allthatmatters/item/custom/GemBow.java
+++ src/main/java/jiraiyah/allthatmatters/item/custom/GemBow.java
@@ -21,52 +21,63 @@ public class GemBow extends RangedWeaponItem implements Vanishable
     public static final int TICKS_PER_SECOND = 20;
     public static final int RANGE = 15;
 
-    public static final float ANIMATION_DURATION_DIVIDER = 5F;
+    public static final float ANIMATION_DURATION_DIVIDER = 5f;
 
     public GemBow(Settings settings)
     {
         super(settings);
     }
 
-    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
-        if (user instanceof PlayerEntity playerEntity) {
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks)
+    {
+        if (user instanceof PlayerEntity playerEntity)
+        {
             boolean bl = playerEntity.getAbilities().creativeMode || EnchantmentHelper.getLevel(Enchantments.INFINITY, stack) > 0;
             ItemStack itemStack = playerEntity.getProjectileType(stack);
             if (!itemStack.isEmpty() || bl) {
-                if (itemStack.isEmpty()) {
+                if (itemStack.isEmpty())
+                {
                     itemStack = new ItemStack(Items.ARROW);
                 }
 
                 int i = this.getMaxUseTime(stack) - remainingUseTicks;
                 float f = getPullProgress(i);
-                if (!((double)f < 0.1)) {
+                if (!((double)f < 0.1))
+                {
                     boolean bl2 = bl && itemStack.isOf(Items.ARROW);
-                    if (!world.isClient) {
+                    if (!world.isClient)
+                    {
                         ArrowItem arrowItem = (ArrowItem)(itemStack.getItem() instanceof ArrowItem ? itemStack.getItem() : Items.ARROW);
                         PersistentProjectileEntity persistentProjectileEntity = arrowItem.createArrow(world, itemStack, playerEntity);
                         persistentProjectileEntity.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getYaw(), 0.0F, f * 3.0F, 1.0F);
-                        if (f == 1.0F) {
+                        if (f == 1.0F)
+                        {
                             persistentProjectileEntity.setCritical(true);
                         }
 
                         int j = EnchantmentHelper.getLevel(Enchantments.POWER, stack);
-                        if (j > 0) {
+                        if (j > 0)
+                        {
                             persistentProjectileEntity.setDamage(persistentProjectileEntity.getDamage() + (double)j * 0.5 + 0.5);
                         }
 
                         int k = EnchantmentHelper.getLevel(Enchantments.PUNCH, stack);
-                        if (k > 0) {
+                        if (k > 0)
+                        {
                             persistentProjectileEntity.setPunch(k);
                         }
 
-                        if (EnchantmentHelper.getLevel(Enchantments.FLAME, stack) > 0) {
+                        if (EnchantmentHelper.getLevel(Enchantments.FLAME, stack) > 0)
+                        {
                             persistentProjectileEntity.setOnFireFor(100);
                         }
 
-                        stack.damage(1, playerEntity, (p) -> {
+                        stack.damage(1, playerEntity, (p) ->
+                        {
                             p.sendToolBreakStatus(playerEntity.getActiveHand());
                         });
-                        if (bl2 || playerEntity.getAbilities().creativeMode && (itemStack.isOf(Items.SPECTRAL_ARROW) || itemStack.isOf(Items.TIPPED_ARROW))) {
+                        if (bl2 || playerEntity.getAbilities().creativeMode && (itemStack.isOf(Items.SPECTRAL_ARROW) || itemStack.isOf(Items.TIPPED_ARROW)))
+                        {
                             persistentProjectileEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
                         }
 
@@ -74,9 +85,11 @@ public class GemBow extends RangedWeaponItem implements Vanishable
                     }
 
                     world.playSound((PlayerEntity)null, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.PLAYERS, 1.0F, 1.0F / (world.getRandom().nextFloat() * 0.4F + 1.2F) + f * 0.5F);
-                    if (!bl2 && !playerEntity.getAbilities().creativeMode) {
+                    if (!bl2 && !playerEntity.getAbilities().creativeMode)
+                    {
                         itemStack.decrement(1);
-                        if (itemStack.isEmpty()) {
+                        if (itemStack.isEmpty())
+                        {
                             playerEntity.getInventory().removeOne(itemStack);
                         }
                     }
@@ -87,7 +100,8 @@ public class GemBow extends RangedWeaponItem implements Vanishable
         }
     }
 
-    public static float getPullProgress(int useTicks) {
+    public static float getPullProgress(int useTicks)
+    {
         float f = (float)useTicks / ANIMATION_DURATION_DIVIDER;
         f = (f * f + f * 2.0F) / 3.0F;
         if (f > 1.0F) {
@@ -97,20 +111,25 @@ public class GemBow extends RangedWeaponItem implements Vanishable
         return f;
     }
 
-    public int getMaxUseTime(ItemStack stack) {
-        return (int)(72000 / ANIMATION_DURATION_DIVIDER);
+    public int getMaxUseTime(ItemStack stack)
+    {
+        return 200;
     }
 
     public UseAction getUseAction(ItemStack stack) {
         return UseAction.BOW;
     }
 
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand)
+    {
         ItemStack itemStack = user.getStackInHand(hand);
         boolean bl = !user.getProjectileType(itemStack).isEmpty();
-        if (!user.getAbilities().creativeMode && !bl) {
+        if (!user.getAbilities().creativeMode && !bl)
+        {
             return TypedActionResult.fail(itemStack);
-        } else {
+        }
+        else
+        {
             user.setCurrentHand(hand);
             return TypedActionResult.consume(itemStack);
         }
